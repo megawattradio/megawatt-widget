5cd260293522ec65250b2be7bb8dab9bf5dbb4ac
megawattradio <info@megawattradio.co.uk>
2026-08-04 13:50:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ async function u() {
         const d = await r.json();
 
         song.textContent = d.now_playing.song.title || 'Unknown Track';
-        artist.textContent = d.now_playing.song.artist || 'MegaWatt Radio';
+        artist.textContent = d.now_playing.song.artist || '';
 
         if (d.live && d.live.is_live) {
             status.textContent = 'LIVE';
             show.textContent = d.live.stream_title || 'Live Show';
         } else {
-            status.textContent = 'ON AIR';
-            show.textContent = 'MegaWatt Radio';
+            status.textContent = 'PLAYLIST';
+            show.textContent = d.now_playing.playlist || '';
         }
 
     } catch (e) {
